94fc3c3ed7d0bb3a78e05f466780735630c67a05
KonstantinKharkov <kprint.kharkov@gmail.com>
2021-03-23 17:00:46 +0200

    end of 13 lesson

--- index.js
+++ index.js
@@ -1,5 +1,67 @@
+
+function getValue(id) {
+    var el = document.getElementById(id);
+    return el.value;
+}
+
+
+
+var firstNameId = 'first-name';
+
+
+
+ window.alert(firstNameId);
+
+
+
+debugger;
+function alertFirstNameValues() {
+    console.log(firstNameEl.value);
+    console.log(firstNameEl.className);
+
+}
+
+function alertLastNameValues() {
+    console.log(lastNameEl.value);
+    console.log(lastNameEl.className);
+}
+
+function alertAddressValues() {
+    console.log(addressEl.value);
+    console.log(addressEl.className);
+}
+
+function alertHobbiesValues(elementParameter) {
+    
+    console.log(elementParameter.value);
+    console.log(elementParameter.className);
+    
+}
+
+
+
+
+
 var firstNameId = 'first-name';
 var firstNameEl = document.getElementById(firstNameId);
-firstNameEl.value = 'new name';
+alertHobbiesValues(firstNameEl);
+
+var lastNameId = 'last-name';
+var lastNameEl = document.getElementById(lastNameId);
+alertHobbiesValues(lastNameEl);
 
 
+var addressId = 'address';
+var addressEl = document.getElementById(addressId);
+
+
+
+
+var citiesId = 'cities';
+var citiesEl = document.getElementById(citiesId);
+
+ 
+var hobbiesId = 'hobbies';
+var hobbiesEl = document.getElementById(hobbiesId);
+
+alertHobbiesValues(hobbiesEl);
